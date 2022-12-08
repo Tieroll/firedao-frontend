db76e6134c9ddd5a4d31c60bbd2ac466e73c8eea
Tieroll <bschm5633218w2yrs@gmail.com>
2022-12-08 09:29:29 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {Routes, Route, Link} from "react-router-dom";
+import React from "react";
+import "./firepoint/script"
+import DaoHome from "./view/daoHome";
+import FAQ from "./view/FAQ";
+import RoadMap from "./view/roadMap";
+import TeamManifest from "./view/TeamManifest";
+import {useTranslation} from "react-i18next";
+
+
+// page uses the hook
+function App() {
+    const {t, i18n} = useTranslation();
+    if(!localStorage.getItem("language")){
+        i18n.changeLanguage('en');
+    }
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<DaoHome/>}/>
+                <Route path="/FAQ" element={<FAQ/>}/>
+                <Route path="/RoadMap" element={<RoadMap/>}/>
+                <Route path="/TeamManifest" element={<TeamManifest/>}/>
+            </Routes>
+        </div>
+    );
+}
+
+
+// here app catches the suspense from page in case translations are not yet loaded
+export default App;
---
+++ src/view/roadMap.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useRef, useState} from 'react';
+import styled from "styled-components";
+import {useTranslation} from 'react-i18next'
+import Header from "../components/header";
+import Footer from "../components/footer";
+function Roadmap(props) {
+    const {t, i18n} = useTranslation();
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        window.location.reload()
+    };
+
+    const [language, setLanguage] = useState(localStorage.getItem("language") ? localStorage.getItem("language") : "English");
+    const [isShowChooseLang, setIsShowChooseLang] = useState(false);
+    const Roadmap = styled.div`
+      overflow: hidden;
+      background: #000;
+      color: #fff;
+      .content{
+        min-height: 60vh;
+        padding-top: 100px;
+        text-align: center;
+        font-size: 30px;
+      }
+    `
+
+    return (
+        <Roadmap>
+            <Header></Header>
+            <div className="content">
+                {t('Under construction, please be patient')}
+            </div>
+            <Footer></Footer>
+        </Roadmap>
+    )
+}
+
+export default Roadmap
